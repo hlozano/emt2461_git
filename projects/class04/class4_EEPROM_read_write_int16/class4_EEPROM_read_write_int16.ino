773a72db38a0303ab808d54d26af72e2c728b814
hernanatcladdagh <hernan@claddaghelectronics.com>
2020-10-17 11:42:38 -0400

    modified the analog IN_OUT example

--- projects/class04/class4_EEPROM_read_write_int16/class4_EEPROM_read_write_int16.ino
+++ projects/class04/class4_EEPROM_read_write_int16/class4_EEPROM_read_write_int16.ino
@@ -14,68 +14,65 @@ byte value = 0;
 * store negative numbers, making their range from 0 to 65536 (2^16 - 1)
 */
 static union {
-		unsigned char mybytes[2];		// 2- 8 bit bytes (We could use data type "byte")
-		unsigned int myint;				// 1-16 bit word
-	};
-
-
-
+    unsigned char mybytes[2];   // 2 - 8 bit bytes (We could use data type "byte")
+    unsigned int myinteger;       // 1 - 16 bit word
+  };
 
 void setup()
-{		//if data is initialized we will have a 56 in first location (our choice)
-	Serial.begin(9600);
-	ee_address = 0;
-	value = EEPROM.read(ee_address);
-	if(value != 99)	// Memory was not initialized
-	{
-		Serial.println("data was not initialized");
-		EEPROM.write(ee_address,99); // make sure I leave a mark knowing I was here
-		myint = 30000; // 30000
-		ee_address++;
-		EEPROM.write(ee_address,mybytes[0]);
-		ee_address++;
-		EEPROM.write(ee_address,mybytes[1]);
+{   //if data is initialized we will have a 56 in first location (our choice)
+  Serial.begin(9600);
+  ee_address = 0;
+  value = EEPROM.read(ee_address);
+  if(value != 99) // Memory was not initialized
+  {
+    Serial.println("data was not initialized");
+    EEPROM.write(ee_address,99); // make sure I leave a mark knowing I was here
+    myinteger = 30000; // 30000
+    ee_address++;
+    EEPROM.write(ee_address,mybytes[0]);
+    ee_address++;
+    EEPROM.write(ee_address,mybytes[1]);
 
-		Serial.println("data was NOT initialized. We saved a 99 in ee_adress 0");
-		while(1);//intentional
-	}
-	else
-		Serial.println("data was already initialized. We read a 99 in ee_adress 0");	
+    Serial.println("data was NOT initialized. We saved a 99 in ee_adress 0");
+    while(1);//intentional
+  }
+  else
+  {
+    Serial.println("data was already initialized. We read a 99 in ee_adress 0");  
+  }
 }
 
 void loop()
 {
-	ee_address = 0;
-	//READ MEMORY
-	value = EEPROM.read(ee_address);
-	ee_address++;
-	mybytes[0] = EEPROM.read(ee_address); // ee_address 1
-	ee_address++;
-	mybytes[1] = EEPROM.read(ee_address); //ee_address 2
+  ee_address = 0;
+  //READ MEMORY
+  value = EEPROM.read(ee_address);
+  ee_address++;
+  mybytes[0] = EEPROM.read(ee_address); // ee_address 1
+  ee_address++;
+  mybytes[1] = EEPROM.read(ee_address); //ee_address 2
 
-	myint++; // SAME AS THE OTHER PROGRAM WHEN WE DID ... value++
+  myinteger++; // SAME AS THE OTHER PROGRAM WHEN WE DID ... value++
 
-	Serial.print(ee_address);
-	Serial.print("\t");
-	Serial.print(value, DEC);
-	Serial.println();
-	
-	Serial.print(ee_address);
-	Serial.print("\t");
-	Serial.print(myint, DEC);
-	Serial.println();
+    Serial.print("initialization value    ");
+  Serial.print(value, DEC);
+  Serial.println();
+  
+  Serial.print("stored value         ");
+  Serial.print(myinteger, DEC);
+  Serial.println();
 
-	ee_address = 1;
-	EEPROM.write(ee_address,mybytes[0]);
-	ee_address++;
-	EEPROM.write(ee_address,mybytes[1]);
+  ee_address = 1;
+  EEPROM.write(ee_address,mybytes[0]);
+  ee_address++;
+  EEPROM.write(ee_address,mybytes[1]);
 
-	
+  
 
-	while(1) // locks the program
-	{
-		//program never leaves
-	}
+  while(1) // locks the program
+  {
+    //program never leaves
+  }
 
 }
 
--- projects/class05/class5_Analog_IN_OUT/class5_Analog_IN_OUT.ino
+++ projects/class05/class5_Analog_IN_OUT/class5_Analog_IN_OUT.ino
@@ -1,24 +1,34 @@
-int LED_Analog_output_Pin = 9;      // LED connected to digital pin 9
-int analog_input_Pin = 3;   // potentiometer connected to analog pin 3
-int val = 0;         // variable to store the read value
 /*
-Syntax
-analogWrite(pin, value)
-Parameters
-pin: the pin to write to.
-value: the duty cycle: between 0 (always off) and 255 (always on).
+  This program outputs anlog voltage to pin9 of the Arduino 
+  Voltage varies based on the input voltage on pin A3
 */
+int LED_Analog_output_Pin = 9;
+int analog_input_Pin = A3;
+
+int input_value = 0;
+int output_value = 0;
+
 void setup()
 {
-  Serial.begin(9600);          //  setup serial
-  pinMode(LED_Analog_output_Pin, OUTPUT);   // sets the pin as output
+
 }
+
 void loop()
 {
-  val = analogRead(analog_input_Pin);   // read the input pin
-  //if value is 1007.... then the output will be 1007 / 4 = 251
-  analogWrite(LED_Analog_output_Pin, val / 4);  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-  Serial.println('analog input = ',val);  // debug value
-  Serial.println('analog output = ',val / 4);  // debug value
-  delay(1000);
+  // turn the LED on (HIGH is the voltage level)
+  Serial.begin(9600);
+  input_value = analogRead(analog_input_Pin);
+   
+  Serial.print("input_value=");
+  Serial.println(input_value);
+  //if output_value is 1007.... then the output will be 1007 / 4 = 251
+  output_value = input_value / 4;
+  
+  Serial.print("output_value=");
+  Serial.println(output_value);
+  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255  
+  analogWrite(LED_Analog_output_Pin, output_value);  
+
+  delay(100); // Wait for 1000 millisecond(s)
+
 }
