6c82b64ff68b62f325423cadac77fd9f47f383cb
hernanatcladdagh <hernan@claddaghelectronics.com>
2021-10-23 11:57:43 -0400

    Added classes 9

--- projects/class05/class5_Servo_Motor/class5_Servo_Motor.ino
+++ projects/class05/class5_Servo_Motor/class5_Servo_Motor.ino
@@ -31,3 +31,47 @@ void loop (void)
 		delay(10000); // waits 10 s between servo commands
 }
 */
+
+/*
+
+
+
+#include <Servo.h>
+Servo myservo; // create servo object to control a servo
+int angle = 0; // variable to store the servo position
+
+void setup()
+{
+	myservo.attach(9); // attaches the servo on pin 9 to the servo object
+  	myservo.write(90);
+  	delay(2000);
+}
+
+void loop()
+{
+  
+	for(angle = 90; angle < 180; angle += 1) // goes from 0 degrees to 180 degrees
+	{ // in steps of 1 degree
+		myservo.write(angle); // tell servo to go to position in variable 'angle'
+		delay(20); // waits 20ms between servo commands
+	}
+	for(angle = 180; angle >= 90; angle -= 1) // goes from 180 degrees to 0 degrees
+	{
+		myservo.write(angle); // move servo in opposite direction
+		delay(20); // waits 20ms between servo commands
+	}
+  	delay(500);
+	for(angle = 90; angle >= 0; angle -= 1) // goes from 180 degrees to 0 degrees
+	{
+		myservo.write(angle); // move servo in opposite direction
+		delay(20); // waits 20ms between servo commands
+	}  
+  	for(angle = 0; angle < 90; angle += 1) // goes from 0 degrees to 180 degrees
+	{ // in steps of 1 degree
+		myservo.write(angle); // tell servo to go to position in variable 'angle'
+		delay(20); // waits 20ms between servo commands
+	}
+  	delay(5000);
+
+  
+}*/
